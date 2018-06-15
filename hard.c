2cd6c677918a5d1dae6c5b81690af2f6c16e4b71
eory96 <21500057@handong.edu>
2018-06-15 22:32:00 +0900

    new file

--- hard.c
+++ hard.c
@@ -5,6 +5,7 @@ int main(){
 	char str1[20];
 	char str2[20];
 	char str3[20];
+	char str4[60];
 
 	printf("첫 번째 문자열 입력 : ");
 	fgets(str1,sizeof(str1),stdin);
@@ -13,9 +14,15 @@ int main(){
 	printf("두 번째 문자열 입력 : ");
         fgets(str2,sizeof(str2),stdin);
         str1[strlen(str2)-1]=0;
+
+	printf("두 번째 문자열 입력 : ");
+        fgets(str3,sizeof(str3),stdin);
+        str1[strlen(str3)-1]=0;
 	
-	strcpy(str3,str1);
-	strcat(str3,str2);
-	printf("조합의 결과 : %s \n",str3);
+	strcpy(str4,str1);
+	strcat(str4,str2);
+	strcat(str4,str3);
+
+	printf("조합의 결과 : %s \n",str4);
 	return 0;
 }
